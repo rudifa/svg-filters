36595d84caab036ecc02f6d1b7cfd4e6b1b78175
Rudi Farkas <rudi.farkas@gmail.com>
2020-04-21 20:08:36 +0200

    add FilterPenta

--- src/svg-filter-demo.js
+++ src/svg-filter-demo.js
@@ -47,10 +47,76 @@ class FilterBlend1 extends LitElement {
             <circle filter="url(#blend-it)" cx="750" cy="750" r="500"/>
             <rect x="250" y="250" width="1000" height="1000" style="fill: none; stroke: black"></rect>
 
+            <path id="triangle120" />
         </svg>
       </div>
     `;
   }
 }
 
+class FilterPenta extends LitElement {
+
+  render() {
+    return html`
+    <div>
+    <svg width="100%" height="100%" viewbox="0 0 1500 1500"> 
+        <g transform="translate(750,750) rotate(30)">
+          <circle r="500" style="fill:none;stroke:black"/>
+          <g transform="rotate(0)">
+          <path d="M0 0 l 250 -433 250 144.3 0 577.3 -250 144.3 Z" style="fill:none;stroke:red" />
+          </g>
+          <g transform="rotate(120)">
+          <path d="M0 0 l 250 -433 250 144.3 0 577.3 -250 144.3 Z" style="fill:none;stroke:green" />
+          </g>
+          <g transform="rotate(240)">
+          <path d="M0 0 l 250 -433 250 144.3 0 577.3 -250 144.3 Z" style="fill:none;stroke:blue" />
+          </g>  
+        </g>  
+      </svg>
+    </div>
+    `;
+  }
+}
+
 customElements.define('filter-blend1', FilterBlend1);
+customElements.define('filter-penta', FilterPenta);
+
+// returns array of [x,y] absolute coordinates for a pentagon
+function pentagonCoords(r = 1.0) {
+  let rpd = Math.PI / 180
+  let c = Math.cos(60*rpd)
+  let s = Math.sin(60*rpd)
+  let out = []
+    out.push([0,0])
+    out.push([c,-s])
+    out.push([1,-(2*s - 1/s)])   
+    out.push([1,(2*s - 1/s)])     
+    out.push([c,s])   
+    out.push([0,0])
+  return out.map(x => x.map(y => r*y))
+}
+
+// returns array of [x,y] successive differences
+function diff(A) {return A.slice(1).map((n, i) => [ n[0] - A[i][0], n[1] - A[i][1] ] )};
+
+/**
+ * > pc500 = pentagonCoords(500)
+[
+  [ 0, 0 ],
+  [ 250.00000000000006, -433.0127018922193 ],
+  [ 500, -288.67513459481273 ],
+  [ 500, 288.67513459481273 ],
+  [ 250.00000000000006, 433.0127018922193 ],
+  [ 0, 0 ]
+]
+> diff(pc500)
+[
+  [ 250.00000000000006, -433.0127018922193 ],
+  [ 249.99999999999994, 144.33756729740657 ],
+  [ 0, 577.3502691896255 ],
+  [ -249.99999999999994, 144.33756729740657 ],
+  [ -250.00000000000006, -433.0127018922193 ]
+]
+> 
+
+ */
